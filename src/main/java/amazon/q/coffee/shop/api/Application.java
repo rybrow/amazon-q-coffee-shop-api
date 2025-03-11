fafde436016b76c6381f4ce0b87e38c18e99c9ce
rybrow <r.s.b@hotmail.co.nz>
2025-03-11 21:43:07 +1300

    Initial commit

---
+++ src/main/java/amazon/q/coffee/shop/api/Application.java
@@ -0,0 +1,10 @@
+package amazon.q.coffee.shop.api;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
